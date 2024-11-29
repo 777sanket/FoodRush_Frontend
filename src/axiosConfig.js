ff530fd51b97998765a7fd799c12b61617e384f9
777sanket <sanketwasnik1902@gmail.com>
2024-11-29 20:54:29 +0530

    feat: 4th-commit

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
+// axios.defaults.baseURL =
+//   // process.env.NODE_ENV !== "production" ? "http://localhost:5001" : "/";
+//   process.env.NODE_ENV !== "production"
+//     ? "https://foodrush-backend-sert.onrender.com"
+//     : "/";
+
 axios.defaults.baseURL =
-  // process.env.NODE_ENV !== "production" ? "http://localhost:5001" : "/";
-  process.env.NODE_ENV !== "production"
-    ? "https://foodrush-backend-sert.onrender.com"
-    : "/";
+  process.env.NODE_ENV === "production"
+    ? "https://foodrush-backend-sert.onrender.com" // Use full backend URL in production
+    : "http://localhost:5001"; // Use local backend in development
 
 console.log("NODE_ENV:", process.env.NODE_ENV);
 console.log("Axios Base URL:", axios.defaults.baseURL);
