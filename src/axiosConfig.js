2616f1498e59e17b1d9032a1c8a75985b0a03bc3
777sanket <sanketwasnik1902@gmail.com>
2024-11-29 20:50:04 +0530

    feat: 3rd-commit

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== "production" ? "http://localhost:5001" : "/";
-// process.env.NODE_ENV !== "production"
-//   ? "https://foodrush-backend-sert.onrender.com"
-//   : "/";
+  // process.env.NODE_ENV !== "production" ? "http://localhost:5001" : "/";
+  process.env.NODE_ENV !== "production"
+    ? "https://foodrush-backend-sert.onrender.com"
+    : "/";
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("Axios Base URL:", axios.defaults.baseURL);
 // process.env.NODE_ENV !== "production"
 // ? "https://food-rush-backend.vercel.app"
 //   : "https://food-rush-backend.vercel.app";
